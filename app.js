100fe43564b3f79f0277c1e8676b5f12bc65fd6f
Mark Bennett <mwbennett@wesleyan.edu>
2015-04-27 13:14:09 -0700

    added function skeletons and tested playFrequency and playInterval

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 (function () {
   
+  // HELPERS 
+  var generateRandomFrequency = function() {
+
+  };
+
+  var generateInterval = function() {
+
+  };
+
+  var playFrequency = function(freq) {
+    var sine = T("sin", {freq: freq, mul:0.5});
+    T("perc", {r:500}, sine).on("ended", function() {
+      this.pause();
+    }).bang().play();
+  };
+  
+  var playInterval = function(arr) {
+    playFrequency(arr[0]);
+    setTimeout(playFrequency(arr[1]), 300);
+  };
+
+  var getNameFromKey = function(keyNum) {
+    return 
+  };
+
+  // APP 
 
 }());

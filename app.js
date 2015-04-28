1104092f9c44a6d65eb16daa83b4c6bd7bdca013
Mark Bennett <mwbennett@wesleyan.edu>
2015-04-28 15:43:01 -0700

    edited server port with environmental variable

--- app.js
+++ app.js
@@ -11,5 +11,7 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+var port = process.env.PORT || 4568;
+
 console.log('Shortly is listening on 4568');
-app.listen(4568);
+app.listen(port);

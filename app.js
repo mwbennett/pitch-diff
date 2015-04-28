e47abaef0af63f6ca080e0f4c040055cdc2b9e81
Mark Bennett <mwbennett@wesleyan.edu>
2015-04-27 21:02:23 -0700

    added basic styles, added raphael and vexflow to render notes on staff, implemented renderInterval function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 
   
-  // HELPERS 
+  //===============================//
+  // HELPERS                       //
+  //===============================//
   var generateRandomNote = function() {
-    var key = Math.floor(Math.random() * 76);
-    return teoria.note.fromKey(key + 6);
+    var key = Math.floor(Math.random() * 64);
+    return teoria.note.fromKey(key + 12);
   };
 
   var generateInterval = function() {
@@ -54,31 +56,87 @@ $(document).ready(function () {
     return intName === parseIntervalString(currentInterval[0].toString()) ? true : false;
   };
 
+  var renderInterval = function(interval) {
+    var canvas = $("canvas")[0];
+    var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
+    var ctx = renderer.getContext();
+    var stave = new Vex.Flow.Stave(10, 0, 200);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var note1 = interval[1];
+    var note2 = interval[2];
+
+    var arr = [note1.toString(), note2.toString()];
+    var noteKeys = [];
+
+    arr.forEach(function(elem){
+      var len = elem.length;
+      elem = elem.slice(0, len - 1) + '/' + elem.slice(len - 1);
+      noteKeys.push(elem);
+    });
+
+    console.log(noteKeys);
+
+    stave.addClef("bass").setContext(ctx).draw();
+
+    // Create the notes
+    var notes = [
+      new Vex.Flow.StaveNote({ keys: noteKeys, duration: "w" })
+        // addAccidental(1, new Vex.Flow.Accidental("b")).
+        // addAccidental(2, new Vex.Flow.Accidental("#"))
+    ];
+
+    // Create a voice in 4/4
+    var voice = new Vex.Flow.Voice({
+      num_beats: 4,
+      beat_value: 4,
+      resolution: Vex.Flow.RESOLUTION
+    });
+
+    // Add notes to voice
+    voice.addTickables(notes);
+
+    // Format and justify the notes to 500 pixels
+    var formatter = new Vex.Flow.Formatter().
+      joinVoices([voice]).format([voice], 500);
+
+    // Render voice
+    voice.draw(ctx, stave);
+          
+  };
+
   var setNewInterval = function() {
     currentInterval = generateInterval();
     var intervalString = parseIntervalString(currentInterval[0].toString());
     if (intervalString.length > 2) {
       setNewInterval();
     }
+    renderInterval(currentInterval);
   }
 
-  // click handlers 
+  setNewInterval();
+
+//===============================//
+// CLICK HANDLERS                //
+//===============================// 
+
   $('.interval-menu').on('click', '.interval-button', function(e) {
     e.preventDefault();
     console.log("Check response: ", checkResponse($(this).text()));
     if (checkResponse($(this).text())) {
-      $(this).addClass('correct');
-      setNewInterval();
+      $(this).addClass('btn-success');
+      $('.new-interval').addClass('btn-info');
       $('.new-interval').text('Play next');
     } else {
-      $(this).addClass('incorrect');
+      $(this).addClass('btn-danger');
     }
   });
 
   $('.playing-options').on('click', '.new-interval', function(e) {
     e.preventDefault();
     $(this).text('New interval');
-    $('.interval-button').removeClass('correct incorrect');
+    $(this).removeClass('btn-info');
+    $('.interval-button').removeClass('btn-danger btn-success');
     $('.play-again').text('Play again');
     setNewInterval();
     playInterval(currentInterval);
